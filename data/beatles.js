492c5ff2a22f3241e84e5b145f3b94add5a27a62
al-abbas-nz <al.mus930@gmail.com>
2020-11-05 19:18:20 +1300

    add 8 songs data

--- data/beatles.js
+++ data/beatles.js
@@ -2,22 +2,76 @@ const beatles = {
   songs: [
     {
       id: 1,
-      albumName: 'Yellow Submarine',
-      songName: 'Yellow Submarine',
+      albumName: 'The Beatles (Self-Titled)',
+      songName: 'Julia',
+      year: '1968',
+      image: 'https://img.discogs.com/1Mkxmm4EH4jut-WSXntEb2bPkac=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-10949698-1509598213-3172.jpeg.jpg',
+      audio: '<iframe width="560" height="315" src="https://www.youtube.com/embed/TZip_br_v3w" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      mood: 'sad'
+    },
+    {
+      id: 2,
+      albumName: 'Revolver',
+      songName: 'Eleanor Rigby',
+      year: '1966',
+      image: 'https://img.discogs.com/WpAqPtfFQP1shlj2slPzZth5jRk=/fit-in/600x599/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2318045-1276592861.jpeg.jpg',
+      audio: '<iframe width="560" height="315" src="https://www.youtube.com/embed/wbxTlxuECJA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      mood: 'sad'
+    },
+    {
+      id: 3,
+      albumName: "Sgt. Pepper's Lonely Hearts Club Band",
+      songName: 'Within You Without You',
+      year: '1967',
+      image: 'https://img.discogs.com/5niM71_Gc3Zkk6VK4cRlpHpQRIY=/fit-in/600x590/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-499497-1456047383-9489.jpeg.jpg',
+      audio: '<iframe width="560" height="315" src="https://www.youtube.com/embed/HsffxGyY4ck" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      mood: 'sad'
+    },
+    {
+      id: 4,
+      albumName: "Revolver",
+      songName: 'And Your Bird Can Sing',
+      year: '1967',
+      image: 'https://img.discogs.com/WpAqPtfFQP1shlj2slPzZth5jRk=/fit-in/600x599/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2318045-1276592861.jpeg.jpg',
+      audio: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Uq0aeEYLkIE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      mood: 'sad'
+    },
+    {
+      id: 5,
+      albumName: 'Abbey Road',
+      songName: 'Here Comes the Sun',
       year: '1969',
-      image: 'www.image.com/',
-      audio: 'www.image.com/',
+      image: 'https://img.discogs.com/OnURySqQ8VzdRxOzztR2RxhD8kA=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2607424-1298729076.jpeg.jpg',
+      audio: '<iframe width="560" height="315" src="https://www.youtube.com/embed/xUNqsfFUwhY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
       mood: 'happy'
     },
     {
-      id: 1,
+      id: 6,
       albumName: 'Yellow Submarine',
-      songName: 'Yellow Submarine',
-      year: '1969',
-      image: 'www.image.com/',
-      audio: 'www.image.com/',
+      songName: 'All You Need Is Love',
+      year: '1967',
+      image: 'https://img.discogs.com/fhKeGE6mBCdFqmvDNOUrr31IKqc=/fit-in/600x604/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-464292-1434665238-3702.jpeg.jpg',
+      audio: '<iframe width="560" height="315" src="https://www.youtube.com/embed/_7xMfIp-irg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
       mood: 'happy'
-    }
+    },
+    {
+      id: 7,
+      albumName: 'Please Please Me',
+      songName: 'I Saw Her Standing There',
+      year: '1967',
+      image: 'https://img.discogs.com/L9f0Ipa8GjhRRL6INcSEKwrXH40=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1350468-1377575562-1715.jpeg.jpg',
+      audio: '<iframe width="560" height="315" src="https://www.youtube.com/embed/oxwAB3SECtc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      mood: 'happy'
+    },
+    {
+      id: 8,
+      albumName: "A Hard Day's Night",
+      songName: "A Hard Day's Night",
+      year: '1964',
+      image: 'https://img.discogs.com/vHOkFygn0OTZRVakq7nkCBWWW9Y=/fit-in/599x595/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-649306-1391254571-9166.jpeg.jpg',
+      audio: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Yjyj8qnqkYI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      mood: 'happy'
+    },
   ]
 }
 
