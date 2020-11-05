858c8f54748d89a20dcac8b12b25e692b225b776
al-abbas-nz <al.mus930@gmail.com>
2020-11-05 19:56:08 +1300

    add material UI

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
 
 import Nav from './Nav'
 import Home from './Home'
@@ -6,6 +7,7 @@ import Home from './Home'
 const App = () => {
   return (
     <>
+      <Button variant ="contained" color="primary">MATERIAL UI! LESHHGOOO</Button>
       <h1>Ringo's Song Selector</h1>
       {/* This 'main' div is only for styling (so we can use flexbox) */}
       <div className='main'>
