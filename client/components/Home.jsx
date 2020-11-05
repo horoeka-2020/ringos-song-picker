1ac80a64c86b52832f62114ff38625a6c696521a
maria-gedye <mariagedye@gmail.com>
2020-11-06 10:28:33 +1300

    skeleton css and assets

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,8 +3,24 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
-      <p>Please select a taxonomic rank from the nav list</p>
+    <div class="box-area">
+      
+      <header>
+        <div class="wrapper">
+          <div class="logo">
+            <a href="https://fontmeme.com/the-beatles-font/"><img src="https://fontmeme.com/permalink/201105/9ca11cf7f585fc322ac101c0a99239e9.png" alt="the-beatles-font" border="0"/></a>
+          </div>
+      
+          <nav>
+            <a href="#">About</a>
+          </nav>
+        </div>
+      </header>
+      
+      <div class="banner-area">
+        <h1 style="font-size:7vmax"> What's the mood?</h1>
+           <a class="border-button" href="#">Happy</a>
+      </div>
     </div>
   )
 }
